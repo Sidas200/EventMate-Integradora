70aa9f6dc5c8d2d101b69f767490b656f52be157
Arturo Sidas <arturo-sidas@hotmail.com>
2024-07-29 11:11:30 -0600

    Se puso lo de index

--- contacto_pagina/contacto.js
+++ contacto_pagina/contacto.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    const navLinks = document.querySelectorAll('.nav-links a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const sectionId = link.getAttribute('href').substring(1);
+            const section = document.getElementById(sectionId);
+            if (section) {
+                window.scrollTo({
+                    top: section.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
     try {
         const response = await fetch("http://localhost:3000/autorizacion", {
             method: "GET",
