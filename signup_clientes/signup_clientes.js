9e19a206f499632fa76e88ed37ab3ea5d4e60324
Arturo Sidas <159062694+Sidas200@users.noreply.github.com>
2024-07-30 21:32:36 -0600

    Revert "Yahir2"

--- signup_clientes/signup_clientes.js
+++ signup_clientes/signup_clientes.js
@@ -1,39 +1,39 @@
-const form = document.getElementById("signup_cliente");
+    const form = document.getElementById("signup_cliente");
 
-form.addEventListener("submit", async(event) => {
-event.preventDefault();
+        form.addEventListener("submit", async(event) => {
+        event.preventDefault();
+    
+        const dataForm = new FormData(form);
+        const data = Object.fromEntries(dataForm);
 
-const dataForm = new FormData(form);
-const data = Object.fromEntries(dataForm);
-
-
-try {
-    const response = await fetch("http://localhost:3000/registrar", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body:JSON.stringify(data),
-        credentials: 'include'
+    
+        try {
+            const response = await fetch("http://localhost:3000/registrar", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body:JSON.stringify(data),
+                credentials: 'include'
+            });
+    
+            if (!response.ok) {
+                throw new Error("Datos no validos");
+            }
+    
+            window.location.href = "../login_usuarios/login_usuario.html";
+        } catch (error) {
+            console.error("Error al registrar al cliente", error);
+            const errorMessageElement = document.getElementById("error-message");
+            errorMessageElement.textContent = error;
+            errorMessageElement.style.display = "block"; 
+    
+            setTimeout(() => {
+                errorMessageElement.style.opacity = "0";
+                setTimeout(() => {
+                    errorMessageElement.style.display = "none";
+                    errorMessageElement.style.opacity = "1"; 
+                }, 1000);
+            }, 1000); 
+        }
     });
-
-    if (!response.ok) {
-        throw new Error("Datos no validos");
-    }
-
-    window.location.href = "../login_usuarios/login_usuario.html";
-} catch (error) {
-    console.error("Error al registrar al cliente", error);
-    const errorMessageElement = document.getElementById("error-message");
-    errorMessageElement.textContent = error;
-    errorMessageElement.style.display = "block"; 
-
-    setTimeout(() => {
-        errorMessageElement.style.opacity = "0";
-        setTimeout(() => {
-            errorMessageElement.style.display = "none";
-            errorMessageElement.style.opacity = "1"; 
-        }, 1000);
-    }, 1000); 
-}
-});
