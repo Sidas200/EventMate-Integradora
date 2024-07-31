34f534201be2cbb5f2add3f21afb634e3eefdba4
ElGuapoEnmascarado <yahirvans11@gmail.com>
2024-07-30 20:54:14 -0600

    Merge branch 'main' of https://github.com/Sidas200/EventMate-Integradora

--- signup_clientes/signup_clientes.js
+++ signup_clientes/signup_clientes.js
@@ -1,40 +1,8 @@
-        
-        
-        const form = document.getElementById("signup_cliente");
+document.getElementById('registroUsuario').addEventListener('click', function() {
+    window.location.href = '../signup_clientes/signup_usuario.html';
+});
 
-        form.addEventListener("submit", async(event) => {
-        event.preventDefault();
-    
-        const dataForm = new FormData(form);
-        const data = Object.fromEntries(dataForm);
+document.getElementById('registroProveedor').addEventListener('click', function() {
+    window.location.href = '../signup_clientes/signup_proveedor.html';
+});
 
-    
-        try {
-            const response = await fetch("http://localhost:3000/clientes", {
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body:JSON.stringify(data),
-            });
-    
-            if (!response.ok) {
-                throw new Error("Datos no validos");
-            }
-    
-            window.location.href = "../login_usuarios/login_usuario.html";
-        } catch (error) {
-            console.error("Error al registrar al cliente", error);
-            const errorMessageElement = document.getElementById("error-message");
-            errorMessageElement.textContent = error;
-            errorMessageElement.style.display = "block"; 
-    
-            setTimeout(() => {
-                errorMessageElement.style.opacity = "0";
-                setTimeout(() => {
-                    errorMessageElement.style.display = "none";
-                    errorMessageElement.style.opacity = "1"; 
-                }, 1000);
-            }, 1000); 
-        }
-    });
