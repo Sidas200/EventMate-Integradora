3b4e06556e836a8467f6563f82a535965b618220
Susana Gallegos <mandalagc@gmail.com>
2024-08-01 18:45:17 -0600

    fin

--- planeador/planeador.js
+++ planeador/planeador.js
@@ -20,11 +20,11 @@ document.addEventListener('DOMContentLoaded', async () => {
             }
         } else {
             console.error("Error al verificar el estado de autenticación");
-            window.location.href = "../login_usuarios/login_usuario.html";
+           // window.location.href = "../login_usuarios/login_usuario.html";
         }
     } catch (error) {
         console.error("Se produjo un error al verificar el estado de autenticación:", error);
-        window.location.href = "../login_usuarios/login_usuario.html";
+        //window.location.href = "../login_usuarios/login_usuario.html";
     }
 
     // Configurar el cierre de sesión
